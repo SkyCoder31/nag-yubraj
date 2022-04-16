dc0717e343711468138663ac5d11051110086866
Yubraj Nag <69618141+SkyCoder31@users.noreply.github.com>
2022-04-16 12:10:50 +0530

    updated

---
+++ src/Components/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#5b186e] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-yellow-500'>
+                      About
+                    </p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+              <div className='sm:text-right text-4xl font-bold '>
+                <p>Hi, have a look around</p>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia illo alias, magnam aliquid perferendis nostrum perspiciatis omnis minus odit atque sint nesciunt labore ipsa ratione! Sapiente, quidem! Vitae, aut vero.
+                </p>
+              </div>
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default About
