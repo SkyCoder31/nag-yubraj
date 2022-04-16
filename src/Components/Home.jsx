fa1dc6c3be82dbc77a52813bdbf1f83353592ae9
Yubraj Nag <69618141+SkyCoder31@users.noreply.github.com>
2022-04-16 12:17:55 +0530

    updated work button function

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { HiArrowCircleRight } from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 const Home = () => {
   return (
@@ -10,7 +10,9 @@ const Home = () => {
         <h2 className='text=3xl sm:text-7xl font-bold text-[#8892b0]'>A Front-End Developer</h2>
         <p className='text-[#ccd6f6] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi nihil, aliquid eius odio quae blanditiis dolorum quaerat facere. Deserunt consectetur nesciunt ex. Iusto dolore architecto nobis quasi quibusdam repellat!</p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-500'>Check out my works 
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-500'><Link to="work" smooth={true} offset={50} duration={500}>
+            Check out my works
+            </Link> 
           <span className='group-hover:rotate-90 duration-200'>
           <HiArrowCircleRight className='ml-3'/>
           </span>
